ea38c3963be212418c7a913833e5c93b5b752336
LightCloud36 <dereko.neil@yahoo.com>
2018-09-26 14:36:41 -0400

    SignUp files
    started with index.html then created api, events, ui, app, and config
    .js files then started new curl script files that include sign-in
    sign-up sign-out .sh

---
+++ assets/auth/api.js
@@ -0,0 +1,14 @@
+'use strict'
+const config = require('../config.js')
+
+const signUp = function (userData) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: userData
+  })
+}
+
+module.exports = {
+  signUp
+}
---
+++ assets/auth/ui.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const signUpSuccess = function () {
+  console.log('sing up successful')
+}
+
+module.exports = {
+  signUpSuccess
+}
