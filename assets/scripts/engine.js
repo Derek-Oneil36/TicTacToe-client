11bacbd5b0d751ccb5ed74e4c90e38328299bfe3
LightCloud36 <dereko.neil@yahoo.com>
2018-09-27 12:55:45 -0400

    engine.js
    added and outcome function that checks for the outcome, winner/tie game

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -30,13 +30,54 @@ const addPlayer = function (post) {
   }
 }
 
-console.log(addPlayer(6))
+const outcome = function () {
+  const draw = 'Draw Play Again!'
 
+  if (board[0] !== '' && board[0] === board[1] && board[1] === board[2]) {
+    return 'The Winner is ' + board[0]
+  } else if (board[3] !== '' && board[3] === board[4] && board[4] === board[5]) {
+    return 'The Winner is ' + board[3]
+  } else if (board[6] !== '' && board[6] === board[7] && board[7] === board[8]) {
+    return 'The Winner is ' + board[6]
+  } else if (board[0] !== '' && board[0] === board[3] && board[3] === board[6]) {
+    return 'The Winner is ' + board[0]
+  } else if (board[1] !== '' && board[1] === board[4] && board[4] === board[7]) {
+    return 'The Winner is ' + board[1]
+  } else if (board[2] !== '' && board[2] === board[5] && board[5] === board[8]) {
+    return 'The Winner is ' + board[2]
+  } else if (board[0] !== '' && board[0] === board[4] && board[4] === board[8]) {
+    return 'The Winner is ' + board[0]
+  } else if (board[2] !== '' && board[2] === board[4] && board[4] === board[6]) {
+    return 'The Winner is ' + board[3]
+  }
+
+  return draw
+}
+// Write a function that checks if someone wins the game
+// const checkForWinner = function() {
+// check for top row winner by seeing if all match and are not empty string
+// if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
+// return 'Winner is ' + board[0]
+//  }
+// check for all other combos
+// middle row
+// bottom row
+// left column
+// middle column
+// right column
+// right to let diagonal
+// left to right diagonal
+// check for tie
+// else no winner yet
+// }
+
+console.log(addPlayer(6))
 console.log(addPlayer(5))
 
 module.exports = {
   board,
   addPlayer,
-  switchPlayer
+  switchPlayer,
+  outcome
 
 }
